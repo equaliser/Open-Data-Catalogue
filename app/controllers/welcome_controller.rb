0a8170872a6899576a428efbd6ecef52d746da84
equaliser <steve@stephenwoodward.co.uk>
2010-03-19 16:48:38 +0000

    Added first names and last names. Also added mysterious photo on homepage.

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -12,9 +12,10 @@ class WelcomeController < ApplicationController
     @title = "Welcome"
     source = "http://abigbang.wordpress.com/feed/"
     content = ""
-    open(source) do |s| content = s.read end
+    raise open(source) do |s| content = s.read end  
     @rss = SimpleRSS.parse(content)
-  
+    rescue
+   
     @latestDatasets = Dataset.latest(5)
   end
   
